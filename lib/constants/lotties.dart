d917777ea2caa7358d71857b494035309bf8862b
Trey Hope <trey.a.hope@gmail.com>
2025-08-04 07:39:28 -0500

    Remove commented-out Lottie animation constants for improved code clarity

--- lib/constants/lotties.dart
+++ lib/constants/lotties.dart
@@ -10,32 +10,20 @@ class Lotties {
   static const chat =
       'https://lottie.host/d2d2b81d-e378-4842-b525-3f0d1bd72897/0z7ilexIZ9.json';
 
-  // static const deleteAccount =
-  //     'https://lottie.host/8b913c18-85c5-463b-b10c-fac2b6929560/GVjMdNNavH.json';
-
   static const groups =
       'https://lottie.host/d05dca70-e470-4284-ad79-4cc78ae8c6fa/2TJKChtOeY.json';
 
   static const leaderboard =
       'https://lottie.host/76d5c302-05ec-4aff-ac6c-3f73832201a9/VBsN3v28u9.json';
 
-  // static const licenses =
-  //     'https://lottie.host/463e1a5d-c740-4ffb-9ee9-83e5f9b00923/VJliIpFOlF.json';
-
   static const linkedAccounts =
       'https://lottie.host/c949d875-6223-4e7e-8eb0-dec46cca9f2b/JN9abpQohW.json';
-
-  // static const logout =
-  //     'https://lottie.host/b9a10e60-1dc2-45a3-aa2f-993b056d76dc/ah7AkC9WGn.json';
 
   static const play =
       'https://lottie.host/afa5e507-7b25-40e2-8cf9-07c7c63bace0/07iIcuc1gh.json';
 
   static const profile =
       'https://lottie.host/ac2d1c3e-cd3d-4463-955a-f71f08033540/49dcqrxHYZ.json';
-
-  // static const signOut =
-  //     'https://lottie.host/f88e459d-6b62-4d0f-a98c-c496fb0c325e/2gJWApc9pg.json';
 
   static const tournament =
       'https://lottie.host/e530bc24-55b7-4718-a153-0e7085a94b50/DKrp8UNP49.json';
